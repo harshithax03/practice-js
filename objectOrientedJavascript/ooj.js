3c11b5bdbd45d7ed848ca95c7414631617b6cd57
harshitha <harshitha.y2022@gmail.com>
2024-10-08 05:12:17 +0530

    class basics

--- objectOrientedJavascript/ooj.js
+++ objectOrientedJavascript/ooj.js
@@ -1,2 +1,32 @@
 const a = 1;
-console.log(a)
+// console.log(a)
+
+// classes basics
+class Math {
+    
+    getSum() {
+        return this.a + this.b
+    }
+    getSubraction() {
+        return this.a - this.b
+    }
+
+    getMutiply() {
+        return this.a *this.b
+    }
+
+    getDivision() {
+        return this.a / this.b
+    }
+}
+ 
+const calculator = new Math()    /* instantiation */
+console.log(calculator.b)  /* results undefined since a /b are not defiined yet but still we are able to access them */
+console.log(calculator.getDivision()) /* returns NaN becuase the a and b are undefined. */
+calculator.a = 15; 
+calculator.b = 14;
+console.log(calculator.getDivision()) /* returns the value. */
+console.log(calculator.getMutiply()) /* returns the value. */
+console.log(calculator.getSum()) /* returns the value. */
+console.log(calculator.getSubraction()) /* returns the value. */
+
--- objectOrientedJavascript/readme.md
+++ objectOrientedJavascript/readme.md
@@ -7,3 +7,14 @@
 * All key values will be stringified
 * properties that doesn't exist will always return undefined instead of showing any kind of errors.
 * the value of an object could be anything, i.e string, number, array, object, function, arrowfunction anything.
+
+#### 2. Class Basics
+
+**Keypoints:** 
+
+* ECMAScript 2015 aka ES6 introduced Javascript Javascript.
+* **Classes :** We define the blueprint which is the class and then we can create objects baesd off that blueprint aka instances.
+* to avoid confusion we initiate the class name with Capital Letters.
+* an instance we created will return as object if we try to find the type of it
+* to check whether it is instace of a particular type we can use instaceof
+* **Constructors :**  is way to initialize properties and is automatically called when there is a new instance of the class.
